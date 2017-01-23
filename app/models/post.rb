b30c9bb941810637d6478d8febd8ef7f5635ec19
lgants <lgants@gmail.com>
2017-01-22 23:47:42 -0600

    added hours requested to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,8 @@ class Post < ApplicationRecord
   # enum allows you to dynamically create methods created based on the contents of the object
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
-  validates_presence_of :date, :rationale
+  validates_presence_of :date, :rationale, :overtime_request
+  validates :overtime_request, numericality: { greater_than: 0.0 }
+
+  scope :posts_by, ->(user) { where(user_id: user.id)}
 end
